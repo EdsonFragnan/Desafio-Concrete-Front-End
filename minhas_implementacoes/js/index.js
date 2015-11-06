2f777e8759bd515b1f249461f03201239f38f713
Edson Luiz Fragnan <edsonl.fragnan@gmail.com>
2015-11-06 12:22:12 -0200

    Final Commit

--- minhas_implementacoes/js/index.js
+++ minhas_implementacoes/js/index.js
@@ -1,24 +1,36 @@
 var app = angular.module('app', []);
 
-app.controller('ctrl', function($scope, $http) {
-    
-    $scope.options = {
-        sort: 'views'
-    };
-    
-    $scope.shots = [];
-    
-    $scope.getShots = function() {
-        var url = 'https://api.dribbble.com/v1/shots?access_token=ee1914ee47ff2dd03b342c66c1b26ab924d95765122eec30dadb56ef72a4a8cc';
-        if($scope.options.sort) url += '&sort='+$scope.options.sort;
-        
-        $http.get(url)
-        .success(function(data) {
-            $scope.shots = data;
+app.controller('controllerAPP', function($scope, $http) {  
+  $scope.options = {
+    sort: 'views'
+  };
+  
+  $scope.shots = [];
+  
+  $scope.obterShots = function() {
+    var url = 'https://api.dribbble.com/v1/shots?access_token=ee1914ee47ff2dd03b342c66c1b26ab924d95765122eec30dadb56ef72a4a8cc';
+    if($scope.options.sort) url += '&sort='+$scope.options.sort;
+    $http.get(url).success(function(data) {
+      for(var texto in data){
+        var objeto = {
+          url: data[texto].html_url,
+          imagem: data[texto].images.normal,
+          descricao: data[texto].description.replace(/<.*?>/g, ''),
+          titulo: data[texto].title
+        };
+        $scope.shots.push({
+          objetoMontado: objeto
         });
-    }
-    
-    $scope.$watchCollection('options', $scope.getShots);
-    
+      }
+    });
+  }
+
+  var tratamento = function(texto) {
+    var textoFormatado = texto.replace(/<.*?>/g, '');
+    return textoFormatado;
+  };
+  
+  $scope.$watchCollection('options', $scope.obterShots);
+  
 });
 
