66c7c23c43223b2209585059bf051fb529416488
Edson Luiz Fragnan <edsonl.fragnan@gmail.com>
2015-11-09 02:56:52 -0200

    Alterações finais

--- minhas_implementacoes/js/index.js
+++ minhas_implementacoes/js/index.js
@@ -12,11 +12,14 @@ app.controller('controllerAPP', function($scope, $http) {
     if($scope.options.sort) url += '&sort='+$scope.options.sort;
     $http.get(url).success(function(data) {
       for(var texto in data){
+        var descricaoTratado = tratamento(data[texto].description);
         var objeto = {
           url: data[texto].html_url,
           imagem: data[texto].images.normal,
-          descricao: data[texto].description.replace(/<.*?>/g, ''),
-          titulo: data[texto].title
+          descricao: descricaoTratado,
+          titulo: data[texto].title,
+          avatar: data[texto].user.avatar_url,
+          nome: data[texto].user.name 
         };
         $scope.shots.push({
           objetoMontado: objeto
@@ -26,8 +29,14 @@ app.controller('controllerAPP', function($scope, $http) {
   }
 
   var tratamento = function(texto) {
-    var textoFormatado = texto.replace(/<.*?>/g, '');
-    return textoFormatado;
+    var textoFormatado;  
+    if (texto == undefined) {
+      var textoFormatado = "Não possui descrição!";
+      return textoFormatado;
+    } else {
+      textoFormatado = texto.replace(/<.*?>/g, '');
+      return textoFormatado;
+    }
   };
   
   $scope.$watchCollection('options', $scope.obterShots);
