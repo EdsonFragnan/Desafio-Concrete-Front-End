92a5dc3cd4d9278daf9ce10cd5c1229e4b288032
Edson Luiz Fragnan <edsonl.fragnan@gmail.com>
2015-11-11 11:22:45 -0200

    Final Commit

--- minhas_implementacoes/js/index.js
+++ minhas_implementacoes/js/index.js
@@ -11,8 +11,8 @@ app.controller('controllerAPP', function($scope, $http) {
     var url = 'https://api.dribbble.com/v1/shots?access_token=ee1914ee47ff2dd03b342c66c1b26ab924d95765122eec30dadb56ef72a4a8cc';
     if($scope.options.sort) url += '&sort='+$scope.options.sort;
     $http.get(url).success(function(data) {
+      console.log(data);
       for(var texto in data){
-        console.log(data);
         var descricaoTratado = tratamento(data[texto].description);
         var objeto = {
           url: data[texto].html_url,
@@ -21,8 +21,12 @@ app.controller('controllerAPP', function($scope, $http) {
           titulo: data[texto].title,
           avatar: data[texto].user.avatar_url,
           nome: data[texto].user.name, 
-          id: data[texto].id
+          id: data[texto].id,
+          views: data[texto].views_count,
+          comentario: data[texto].comments_count,
+          likes: data[texto].likes_count,
         };
+        console.log(objeto);
         $scope.shots.push({
           objetoMontado: objeto
         });
